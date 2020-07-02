cbf5e2cd89eaa02ed2b546185806d3a117095b05
Brenna Good <brenna.elise.good@gmail.com>
2020-07-01 20:24:39 -0700

    initial psuedo code entered

---
+++ jscript.js
@@ -0,0 +1,8 @@
+// Fill in the text for the Jumbotron
+// Assign css styling by past present future time
+// create save button to store users entry to locale storage
+// localStorage.setItem("name", data(customer's comment))
+// The time block they're entering their appointment into needs to contribute to the "name" of our locale storage item
+// value attribute  of the form or button
+// localeStorage.setItem(timeWindow.value)
+// have whatever info is in localestorage show on the webpage
